0352ea51141f8760742ef65ce43bac418b4cddae
manthanmabc <manthanmji@gamil.com>
2021-01-09 15:41:29 +0530

    removed useless stuff

--- bot.js
+++ bot.js
@@ -11,22 +11,27 @@ bot.login(token)
 let killgifs=['https://i.gifer.com/Lpqq.gif', 'https://i.gifer.com/WaBG.gif', 'https://steamuserimages-a.akamaihd.net/ugc/271722715908279502/7F6E3E9EBBAB19EB513CE51015B7632828C2748E/']
 
 bot.on('ready', () => {
+
+const prefix='A'
+
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
   console.log(msg.author.username  + ': ' + msg.content)
   if(msg.author.username!=="Robot Assasian"){
-    if (msg.content === 'ping') {
+    if (msg.content === prefix+'ping') {
        msg.reply('pong');
        msg.channel.send('pong');
     }
     if (msg.content === '*p') {
-      sentimg(msg.channel,"discordjs.png")
+      sentimg(msg.channel,"discs.png")
     }
-    if (msg.content.search("Akill") !== -1) {
-      msg.reply(' killed someone'+killgifs[Math.floor(Math.random() * killgifs.length)])
-      
+    if (msg.content.search(prefix+"kill") !== -1) {
+      msg.reply(' killed someone'+killgifs[Math.floor(Math.random() * killgifs.length)]) 
+    }
+    if (msg.content === '*p') {
+      sentimg(msg.channel,"disrdjs.png")
     }
   }
 });
